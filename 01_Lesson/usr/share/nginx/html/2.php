6aeb77530499482c7fc8e2219144e3234f331893
alexgerasimov58rus <vintegewhitecool@yandex.ru>
2021-01-30 16:52:42 +0300

    Конфиг NGINX, конфиг PHP-FPM и 2 скрипта php

---
+++ 01_Lesson/usr/share/nginx/html/2.php
@@ -0,0 +1,5 @@
+<?php
+    $a= "<a href='index.php'>main</a>";
+    echo $a;
+    echo "<h2>Second</h2>";
+?>
---
+++ 01_Lesson/usr/share/nginx/html/index.php
@@ -0,0 +1,6 @@
+<?php
+    $h1 = "<h1>Main</h1>";
+    $a = "<a href='2.php'>second</a>";
+    echo $h1;
+    echo $a;
+?>
